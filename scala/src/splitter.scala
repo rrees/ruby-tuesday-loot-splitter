ae42d14fc362c4b9be5b796c8b9aa4150e0cedee
unknown <spam@shudderfix.com>
2008-11-25 16:25:56 +0000

    Refactored the splitter to try and make it more readable

--- scala/src/splitter.scala
+++ scala/src/splitter.scala
@@ -5,27 +5,34 @@ package splitter {
 	class LootSplitter
 		
 	object LootSplitter {
+		
+		val emptyBag = List[GemBag]()
+
+		val smallestGemBagFirst = {(firstGemBag: GemBag, secondGemBag: GemBag) => firstGemBag.totalValue < secondGemBag.totalValue}
+
+		val largestGemFirst = {(aGem: Gem, anotherGem: Gem) => aGem.value > anotherGem.value}
+		
 		def splitLoot(gembag: GemBag, shares: Int): List[GemBag] = {
 			
+			
 			if((gembag.totalValue % shares) != 0) {
-				return List[GemBag]()
+				return emptyBag
 			}
 			
 			val individualShareValue = gembag.totalValue / shares
 			
 			var partShares: List[GemBag] = List.make(shares, new GemBag(List()))
 			
-			gembag.gems.sort(_.value > _.value).foreach((gem: Gem) => {
-				assignGemToBag(gem, partShares)
-				partShares = partShares.sort(_.totalValue < _.totalValue)
-				partShares = List(partShares(0) add gem) ::: (partShares drop 1)
+			gembag.gems.sort(largestGemFirst).foreach((gem: Gem) => {
+				partShares = assignGemToBag(gem, partShares)
 			})
 			
 			partShares
 		}
 		
 		private def assignGemToBag(gem:Gem,  bags: List[GemBag]): List[GemBag] = {
-			bags
+			val sortedBags = bags.sort(smallestGemBagFirst)
+			List(sortedBags(0) add gem) ::: (sortedBags drop 1)
 		}
 	}
 }
